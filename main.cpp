78000336fe78be4f1b60836d882f44e81fc82f90
mckeemseattleu <mckeem@seattleu.edu>
2022-02-17 17:28:54 -0800

    sync it

--- main.cpp
+++ main.cpp
@@ -1,7 +1,211 @@
-#include <cstdio>
+// C program for the above approach
 
+#if 0
+#include <stdio.h>
+#include <stdlib.h>
+
+// Initialize a mutex to 1
+int mutex = 1;
+
+// Number of full slots as 0
+int full = 0;
+
+// Number of empty slots as size
+// of buffer
+int empty = 10, x = 0;
+
+// Function to produce an item and
+// add it to the buffer
+void producer()
+{
+    // Decrease mutex value by 1
+    --mutex;
+
+    // Increase the number of full
+    // slots by 1
+    ++full;
+
+    // Decrease the number of empty
+    // slots by 1
+    --empty;
+
+    // Item produced
+    x++;
+    printf("\nProducer produces"
+        "item %d",
+        x);
+
+    // Increase mutex value by 1
+    ++mutex;
+}
+
+// Function to consume an item and
+// remove it from buffer
+void consumer()
+{
+    // Decrease mutex value by 1
+    --mutex;
+
+    // Decrease the number of full
+    // slots by 1
+    --full;
+
+    // Increase the number of empty
+    // slots by 1
+    ++empty;
+    printf("\nConsumer consumes "
+        "item %d",
+        x);
+    x--;
+
+    // Increase mutex value by 1
+    ++mutex;
+}
+
+// Driver Code
 int main()
 {
-    printf("hello from %s!\n", "Producer");
+    int n, i;
+    printf("\n1. Press 1 for Producer"
+        "\n2. Press 2 for Consumer"
+        "\n3. Press 3 for Exit");
+
+    // Using '#pragma omp parallel for'
+    // can  give wrong value due to
+    // synchronization issues.
+
+    // 'critical' specifies that code is
+    // executed by only one thread at a
+    // time i.e., only one thread enters
+    // the critical section at a given time
+#pragma omp critical
+
+    for (i = 1; i > 0; i++) {
+
+        printf("\nEnter your choice:");
+        scanf("%d", &n);
+
+        // Switch Cases
+        switch (n) {
+        case 1:
+
+            // If mutex is 1 and empty
+            // is non-zero, then it is
+            // possible to produce
+            if ((mutex == 1)
+                && (empty != 0)) {
+                producer();
+            }
+
+            // Otherwise, print buffer
+            // is full
+            else {
+                printf("Buffer is full!");
+            }
+            break;
+
+        case 2:
+
+            // If mutex is 1 and full
+            // is non-zero, then it is
+            // possible to consume
+            if ((mutex == 1)
+                && (full != 0)) {
+                consumer();
+            }
+
+            // Otherwise, print Buffer
+            // is empty
+            else {
+                printf("Buffer is empty!");
+            }
+            break;
+
+            // Exit Condition
+        case 3:
+            exit(0);
+            break;
+        }
+    }
+}
+#endif
+
+#include <cstdio>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+/*
+This program provides a possible solution for producer-consumer problem using mutex and
+semaphore.
+I have used 5 producers and 5 consumers to demonstrate the solution. You can always play with these
+values.
+*/
+void delay(int milliseconds)
+{
+    struct timespec ts;
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1000000;
+    nanosleep(&ts, NULL);
+}
+#define MaxItems 5 // Maximum items a producer can produce or a consumer can consume
+#define BufferSize 5 // Size of the buffer
+sem_t empty;
+sem_t full;
+int in = 0;
+int out = 0;
+int buffer[BufferSize];
+pthread_mutex_t mutex;
+void* producer(void* pno)
+{
+    int item;
+    for (int i = 0; i < MaxItems; i++) {
+        item = rand() % 1000; // Produce an random item
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+        buffer[in] = item;
+        printf("Producer %d: Insert Item %d at %d\n", *((int*)pno), buffer[in], in);
+        in = (in + 1) % BufferSize;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+    }
+    return NULL;
+}
+void* consumer(void* cno)
+{
+    //int item;
+    for (int i = 0; i < MaxItems; i++) {
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+        int item = buffer[out];
+        printf("Consumer %d: Remove Item %d from %d\n", *((int*)cno), item, out);
+        out = (out + 1) % BufferSize;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty);
+    }
+    return NULL;
+}
+int main()
+{
+    pthread_t pro[5], con[5];
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&empty, 0, BufferSize);
+    sem_init(&full, 0, 0);
+    int a[5] = { 1,2,3,4,5 }; //Just used for numbering the producer and consumer
+    for (int i = 0; i < 5; i++) {
+        pthread_create(&pro[i], NULL, producer, (void*)&a[i]);
+    }
+    for (int i = 0; i < 5; i++) {
+        pthread_create(&con[i], NULL, consumer, (void*)&a[i]);
+    }
+    for (int i = 0; i < 5; i++) {
+        pthread_join(pro[i], NULL);
+    }
+    for (int i = 0; i < 5; i++) {
+        pthread_join(con[i], NULL);
+    }
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
     return 0;
 }
